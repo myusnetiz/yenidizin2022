18f65128e71347dd9dca4c9e861f1046f642bc4e
Ümit Solmaz <26837921+usnotv@users.noreply.github.com>
2022-10-24 17:17:45 +0300

    Update search.php

--- search.php
+++ search.php
@@ -15,10 +15,7 @@
 					<dd><?php echo $post->post_content; ?></dd>							
 			<?php endwhile; ?>
 			<?php else : ?>
-			<!-- Admatic interstitial 800x600 Ad Code START -->
-<ins data-publisher="adm-pub-149720679927" data-ad-type="interstitial" class="adm-ads-area" data-ad-network="117175119791" data-ad-sid="209" data-ad-width="800" data-ad-height="600"></ins>
-<script src="//cdn2.admatic.com.tr/showad/showad.js" async></script>
-<!-- Admatic interstitial 800x600 Ad Code END -->
+	
 			<h4 class="text-center"><i class='fa fa-exclamation'></i> Sonuç bulunamadı. Başka bir <i class="fa fa-search"></i> arama?</h4>
 			<?php endif; ?>					
 			   </dl>	
