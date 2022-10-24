081f184f57e3925649a806f232e4ed3e8db63dc1
Ümit Solmaz <26837921+usnotv@users.noreply.github.com>
2022-10-24 17:32:11 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -7,10 +7,7 @@
 			</div>
 			<div class="row" id="content">
 			<h4 class="text-center">Sayfa bulunamadı. <i class="fa fa-search"> Arama</i> yapmayı deneyin?</h4>
-			<!-- Admatic interstitial 800x600 Ad Code START -->
-<ins data-publisher="adm-pub-149720679927" data-ad-type="interstitial" class="adm-ads-area" data-ad-network="117175119791" data-ad-sid="209" data-ad-width="800" data-ad-height="600"></ins>
-<script src="//cdn2.admatic.com.tr/showad/showad.js" async></script>
-<!-- Admatic interstitial 800x600 Ad Code END -->
+			
 			</div>
 		</div>
 <?php get_footer(); ?>
